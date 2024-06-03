9e7baf9e01b842d9bc4bb7ea672ef7906f4fcb7d
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2024-06-04 02:05:37 +0530

    React Dependencies & Styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+
+  const [expand, setExpand] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -26,23 +32,31 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function triggerExpand() {
+    setExpand(true);
+  }
+
   return (
+
     <div>
-      <form>
-        <input
+      <form className="create-note">
+        {expand && <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        />}
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={triggerExpand}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={expand ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={expand}>
+          <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
